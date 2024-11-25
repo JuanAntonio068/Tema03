97d8785d1dbc208d53620c0947017ffa975634a3
JuanAntonio068 <ja.montano@iesnervion.es>
2024-11-25 12:48:18 +0100

    Terminado el 1 y emmpezado el 2

---
+++ src/Parte6/Problema01.java
@@ -0,0 +1,83 @@
+package Parte6;
+
+import java.util.*;
+
+public class Problema01 {
+
+	public static void main(String[] args) {
+
+		// Variable donde se va a guardar el número introducido por el usuario
+		int valor;
+
+		// Matriz donde se va a guardar los números introducidos por el usuario
+		int matriz[][];
+
+		// Variable donde se va a guardar el número de filas introducido por el usuario
+		int filas;
+
+		// Variable donde se va a guardar el número de columas introducido por el
+		// usuario
+		int columnas;
+
+		// Variable para marcar si valor introducido está en la tabla
+		boolean esta;
+
+		// Declaramos el escaner
+		Scanner sc = new Scanner(System.in);
+
+		// Le preguntamos al usuario el número de filas
+		System.out.println("Dime el número de filas de la matriz");
+		filas = sc.nextInt();
+
+		// Le preguntamos al usuario el número de columnas
+		System.out.println("Dime el número de columnas de la matriz");
+		columnas = sc.nextInt();
+
+		matriz = new int[filas][columnas];
+
+		// Bucle para asiganr un valor a cada elemento de la matriz
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[0].length; j++) {
+
+				// Preguntamos al usuario por un número
+				System.out.println("Dime un número");
+				matriz[i][j] = sc.nextInt();
+			}
+		}
+		// Preguntamos al usuario por un número para buscarlo
+		System.out.println("Dime un valor para buscarlo en la matriz");
+		valor = sc.nextInt();
+
+		// Llamamos a la función
+		esta = esta(matriz, valor);
+
+		// Imprimimos el resultado
+		System.out.println((esta) ? valor + " está en la tabla" : valor + " no está en la tabla");
+
+		// Cerramos el escaner
+		sc.close();
+	}
+
+	static boolean esta(int[][] matriz, int valor) {
+
+		// Variable para guardar la posicion
+		int index = -1;
+
+		// Buscamos en la tabla
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[0].length; j++) {
+
+				// Iniciamos la comparación
+				if (matriz[i][j] == valor) {// El número está en la matriz
+					index = 1;
+				}
+			}
+		}
+		// Iniciamos la comaparación
+		if (index == 1) {// El número se ha encontrado
+			return true;
+		} else {// El número no se ha encontrado
+			return false;
+		}
+	}
+}
---
+++ src/Parte6/Problema02.java
@@ -0,0 +1,57 @@
+package Parte6;
+
+import java.util.*;
+
+public class Problema02 {
+
+	public static void main(String[] args) {
+
+		// Variable donde se va a guardar la longitud introducida por el usuario
+		int longitud;
+
+		// Tabla que se va a ordenar
+		int tabla[];
+
+		// Declaramos el esacaner
+		Scanner sc = new Scanner(System.in);
+
+		// Preguntamos al usuario por la longitud
+		System.out.println("Dime la longitud de la tabla");
+		longitud = sc.nextInt();
+
+		tabla = new int[longitud];
+
+		// Bucle para asiganr un valor a cada elemento de la tabla
+		for (int i = 0; i < tabla.length; i++) {
+
+			// Preguntamos al usuario por número
+			System.out.println("Dime un número");
+			tabla[i] = sc.nextInt();
+
+			
+			// Cerrmos el escaner
+			sc.close();
+		}
+	}
+
+	static int[] desordenarTabla(int[] tabla) {
+
+		// Variable para guardar una posicion aleatoria
+		int randomPosition;
+
+		// Variable temporal para guardar el valor actual de la tabla
+		int temp;
+		// Declaramos la clase random
+		Random rand = new Random();
+
+		// Bucle para desordenar la tabla
+		for (int i = 0; i < tabla.length; i++) {
+			randomPosition = rand.nextInt(tabla.length);
+			temp = tabla[i];
+			tabla[i] = tabla[randomPosition];
+			tabla[randomPosition] = temp;
+		}
+		return tabla;
+	}
+
+}
